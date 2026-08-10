7e4906f9b90bca620e673d0b358c95b297d75354
Kedar Vijay Marathe <149170262+kedar1330@users.noreply.github.com>
2026-08-10 11:39:12 +0530

    Updating

--- Blood_donation_management_1/models/pre_booking.py
+++
@@ -1,8 +0,0 @@
-class PreBooking:
-
-    def __init__(self, recipient_name, phone, blood_group, quantity, hospital_name):
-        self.recipient_name = recipient_name
-        self.phone = phone
-        self.blood_group = blood_group
-        self.quantity = quantity
-        self.hospital_name = hospital_name
